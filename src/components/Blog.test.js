4bbc0cac206c3b99746cd07cfd3e34561d87281f
jevasa <jevasa@utu.fi>
2024-05-04 14:24:35 +0300

    5.12 completed

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders title', () => {
@@ -35,4 +36,26 @@ test('renders author', () => {
     const div = container.querySelector('.blog')
     expect(div).toHaveTextContent("blogger")
     
+    })
+
+test('clicking the button twice calls event handler twice', async () => {
+    const bloguser ={username:"user"}
+    const blog = {
+        title: "this is my blog",
+        author: "blogger",
+        user: bloguser
+      }
+    
+    const mockHandler = jest.fn()
+    
+    render(
+        <Blog blog={blog} user={bloguser} handleLike={mockHandler}/>
+    )
+    
+    const user = userEvent.setup()
+    const button = screen.getByText('like')
+    await user.click(button)
+    await user.click(button)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
     })
